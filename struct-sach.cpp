3bc842884cb916ee4c5d926f4dac88d9504a2b30
Trung Hoang <hoangphamtrung123tv@gmail.com>
2026-01-30 16:13:33 +0700

    Add files via upload

--- struct-sach.cpp
+++ struct-sach.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 typedef struct
 {
 	char masach[20];
@@ -10,6 +11,8 @@ typedef struct
 	char nhaxuatban[50];
 	char loaisach[30];
 } SACH;
+//khai bao ham tim sach
+void timSach(SACH a[], int n);
 //Nhap 1 quyen sach
 void nhap(SACH *s)
 {
@@ -26,18 +29,19 @@ int main()
     SACH a[10];
     int n, i;
 
-    /* NHAP N SACH */
+    // nhap n sach
     printf("Nhap so sach: ");
     scanf("%d", &n);
 
     for(i = 0; i < n; i++)
         nhap(&a[i]);
 
-    /* XUAT N SACH */
+    //xuar n sach
     printf("\nDanh sach sach vua nhap:\n");
     for(i = 0; i < n; i++)
         xuat(a[i]);
-
+	//goi ham tim sach
+	timSach(a, n);
     return 0;
 }
 	//ghi file
@@ -56,3 +60,29 @@ int main()
 		fread(a, sizeof(SACH), *n, f);
 		fclose(f);
 	}
+	//tim sach
+	void timSach(SACH a[], int n)
+{
+    char ma[20];
+    int found = 0;
+
+    printf("\nNhap ma sach can tim: ");
+    scanf("%s", ma);
+
+    for(int i = 0; i < n; i++)
+    {
+        if(strcmp(a[i].masach, ma) == 0)
+        {
+            printf("\nDa tim thay sach:\n");
+            xuat(a[i]);      // hien thi sach da nhap
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("\nKhong tim thay sach co ma %s\n", ma);
+    }
+}
+
